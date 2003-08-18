118761e55b13f20f96fcaf5c0ee2cd5f513db7dc
golpa <>
2003-08-18 15:56:01 +0000

    Added todo list

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,4 +1,4 @@
-//$Header: /nfs/slac/g/glast/ground/cvs/EbfWriter/src/mainpage.h,v 1.2 2003/08/08 19:25:37 golpa Exp $
+//$Header: /nfs/slac/g/glast/ground/cvs/EbfWriter/src/mainpage.h,v 1.3 2003/08/12 02:47:19 golpa Exp $
 // (Special "header" just for doxygen)
 
 /** @mainpage  package EbfWriter
@@ -15,7 +15,6 @@ packages needed to build the simulation/ reconstruction environment.  This packa
   <ul>
     <li> access to all quantities in the Transient Data Store (TDS)</li>
     <li> writing text output to the log file (which can be turned on/off with the gui "printer").
-    <li> generating histograms and tuples, which would then be written to a ROOT file. (Need to document this.)
   </ul>
 
   @image html EbfWriter.jpg
@@ -28,5 +27,6 @@ packages needed to build the simulation/ reconstruction environment.  This packa
   <hr>
   @section requirements requirements
   @include requirements
-
+  <hr>
+  @todo add real unit test
 */
