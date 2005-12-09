204d5c4c34ebe36901feb868f9f4cb1b68c28200
winer <>
2005-12-09 16:55:40 +0000

    Initial Entry

---
+++ src/FesExposure.h
@@ -0,0 +1,7 @@
+/** 
+* @file ExposureAlg.h
+* @brief fomer definition of ExposureAlg
+* see ExposureAlg.cxx
+*
+*  $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/FesExposure.h,v 1.6 2002/10/23 12:55:52 burnett Exp $
+*/
