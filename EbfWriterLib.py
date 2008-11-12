97f403daf8ecbe28ed1377e638bb1bacf69ba0d1
ecephas <>
2008-11-12 19:34:50 +0000

    Added      env.Tool('TriggerLib')      env.Tool('TkrDigiLib')      env.Tool('astroLib')

--- EbfWriterLib.py
+++ EbfWriterLib.py
@@ -1,4 +1,4 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/EbfWriter/EbfWriterLib.py,v 1.1 2008/07/09 21:13:39 glastrm Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/EbfWriter/EbfWriterLib.py,v 1.1 2008/08/15 21:42:34 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['EbfWriter'])
@@ -6,6 +6,8 @@ def generate(env, **kw):
     env.Tool('guiLib')
     env.Tool('identsLib')
     env.Tool('AcdUtilLib')
-
+    env.Tool('TriggerLib')
+    env.Tool('TkrDigiLib')
+    env.Tool('astroLib')
 def exists(env):
     return 1;
